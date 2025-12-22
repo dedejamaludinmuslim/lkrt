f9f24a2df75124f1b13050eabf34f156f16a8c32
dedejamaludin <96928337+dedejamaludinmuslim@users.noreply.github.com>
2025-12-22 20:22:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,8 @@
 const CACHE_NAME = 'dompet-keluarga-v1';
-// Daftar file yang mau dicache agar loading cepat
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
