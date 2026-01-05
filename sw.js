0ff570dbd8091dfec3e324be6845886a46815218
dedejamaludin <96928337+dedejamaludinmuslim@users.noreply.github.com>
2026-01-05 22:29:46 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,38 @@
-const CACHE_NAME = 'dompet-keluarga-v1';
-const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png'];
+const CACHE_NAME = 'dompet-keluarga-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './splash-ios.png'
+];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting(); // Paksa update SW baru
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Hapus cache v1 yang lama
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
